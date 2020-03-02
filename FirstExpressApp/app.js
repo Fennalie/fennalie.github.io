1791dc8d933e7911e2a0763f862b963ed0332243
Phuong Truong <tarp.stuff@gmail.com>
2020-03-02 16:52:46 +0700

    02March2020
    stuff

---
+++ FirstExpressApp/app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+
+// "/" == Hi there
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+// "/bye" == Goodbye
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+// "/dog" == MEOW
+app.get("/dog", function(req, res){
+    console.log("requested /dog")
+    res.send("MEOW!");
+});
+app.get("/r/:subredditName", function(req, res){
+    var subreddit = req.params.subredditName;
+    console.log(req.params);
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+    console.log(req.params);
+    res.send("SUBR! - comments - id - title");
+});
+// ERROR msg 
+app.get("*", function(req, res){
+    console.log("**")
+    res.send("You are a star!");
+});
+
+// Tell Express to listen for requests
+app.listen(3000, function(){
+    console.log('Server listening on p3000')
+});
--- MyShop/listProducts.js
+++ MyShop/listProducts.js
@@ -3,12 +3,12 @@ var faker = require('faker');
 var loop = 0
 function loopit(){
     while (loop < 10) {
-        console.log(faker.commerce.productName() + "- " + "$" + faker.commerce.price());
+        console.log(faker.commerce.productName() + " - " + "$" + faker.commerce.price());
         loop ++;
     }
 }
 
-console.log("===========================" , "WELCOME TO MY SHOP")
+console.log("=========================== \n\ WELCOME TO MY SHOP \n\===========================")
 loopit();
 
 // 10 random product names and prices
