6cba804984953221f55a553c022b7127ac731dae
Phuong Truong <tarp.stuff@gmail.com>
2021-01-21 17:51:42 +0700

    quiz beta 1.0

--- Game quiz/js/gameQuiz.js
+++ Game quiz/js/gameQuiz.js
@@ -15,18 +15,122 @@ var allButtons = document.querySelectorAll("button")
 var chosen = []
 var answers = []
 var arr = [];
+var winner = []
+var playerInputValue = []
 
 var games = {
-    '1':  { 'name': "Bananagram", 'maxplayers': 8, 'time': "short", 'kid':true, 'competitive':true },
-    '2':  { 'name': "Carcassonne", 'maxplayers': 5, 'time': "long", 'kid':true, 'competitive':true},
-    '3':  { 'name': "Code Names", 'maxplayers': 6, 'time': "short", 'kid':true, 'competitive':false},
-    '4':  { 'name': "Battle ships", 'maxplayers': 2, 'time': "medium", 'kid':true, 'competitive':true},
-    '5':  { 'name': "Aquarius", 'maxplayers': 4, 'time': "short", 'kid':true, 'competitive':true },
-    '6':  { 'name': "Dragon wood", 'maxplayers': 4, 'time': "short", 'kid':true, 'competitive':true},
-    '7':  { 'name': "Guillotine", 'maxplayers': 4, 'time': "short", 'kid':true, 'competitive':true},
-    '8':  { 'name': "Monopoly Deal", 'maxplayers': 4, 'time': "medium", 'kid':true, 'competitive':true},
-    '9':  { 'name': "Pandemic", 'maxplayers': 4, 'time': "medium", 'kid':true, 'competitive':false},    
+    '1':  { 'name': "Aquarius", 'maxplayers': 5,'minplayers': 3,'time': "short", 'kid':true, 'competitive':true },
+    '2':  { 'name': "Avalon", 'maxplayers': 10,'minplayers': 6, 'time': "short", 'kid':false, 'competitive':true},    
+    '3':  { 'name': "Bananagram", 'maxplayers': 8,'minplayers': 4,'time': "short", 'kid':true, 'competitive':true },
+    '4':  { 'name': "Battle ships", 'maxplayers': 2,'minplayers': 2, 'time': "short", 'kid':true, 'competitive':true},
+    '5':  { 'name': "Boggle", 'maxplayers': 10,'minplayers': 3, 'time': "short", 'kid':true, 'competitive':true},    
+    '6':  { 'name': "Carcassonne", 'maxplayers': 5,'minplayers': 2,'time': "medium", 'kid':true, 'competitive':true},
+    '7':  { 'name': "Century:Eastern Wonders", 'maxplayers': 4,'minplayers': 4, 'time': "medium", 'kid':false, 'competitive':false},    
+    '8':  { 'name': "Chess", 'maxplayers': 2,'minplayers': 2,'time': "medium", 'kid':false, 'competitive':true},
+    '9':  { 'name': "Code Names", 'maxplayers': 8,'minplayers': 4,'time': "short", 'kid':true, 'competitive':false},
+    '10':  { 'name': "Coup", 'maxplayers': 6,'minplayers': 5, 'time': "short", 'kid':false, 'competitive':true},    
+    '11':  { 'name': "Deep sea adventures", 'maxplayers': 6,'minplayers': 4, 'time': "short", 'kid':true, 'competitive':true},    
+    '12':  { 'name': "Dixit", 'maxplayers': 6,'minplayers': 3, 'time': "medium", 'kid':true, 'competitive':true},    
+    '13':  { 'name': "Dragon wood", 'maxplayers': 4,'minplayers': 3, 'time': "short", 'kid':true, 'competitive':true},
+    '14':  { 'name': "Dungeon Mayhem", 'maxplayers': 4,'minplayers': 4, 'time': "medium", 'kid':true, 'competitive':true},
+    '15':  { 'name': "Game of Life", 'maxplayers': 6,'minplayers': 4, 'time': "medium", 'kid':true, 'competitive':true},
+    '16':  { 'name': "Guillotine", 'maxplayers': 5,'minplayers': 4, 'time': "short", 'kid':true, 'competitive':true},
+    '17':  { 'name': "Hanabi", 'maxplayers': 5,'minplayers': 4, 'time': "short", 'kid':true, 'competitive':false},
+    '18':  { 'name': "I would fight the dragon", 'maxplayers': 8,'minplayers': 5, 'time': "medium", 'kid':false, 'competitive':true},
+    '19':  { 'name': "Llamas Unleashed", 'maxplayers': 8,'minplayers': 4, 'time': "medium", 'kid':false, 'competitive':true},
+    '20':  { 'name': "Monopoly Deal", 'maxplayers': 5,'minplayers': 4, 'time': "short", 'kid':true, 'competitive':true},
+    '21':  { 'name': "Pandemic", 'maxplayers': 4,'minplayers': 2, 'time': "medium", 'kid':true, 'competitive':false},    
+    '22':  { 'name': "Patchwork", 'maxplayers': 2,'minplayers': 2, 'time': "medium", 'kid':true, 'competitive':true},    
+    '23':  { 'name': "Pictionary", 'maxplayers': 8,'minplayers': 6, 'time': "medium", 'kid':true, 'competitive':false},    
+    '24':  { 'name': "Pictureka", 'maxplayers': 7,'minplayers': 4, 'time': "medium", 'kid':true, 'competitive':true},    
+    '25':  { 'name': "Power Grid", 'maxplayers': 6,'minplayers': 4, 'time': "long", 'kid':false, 'competitive':true},    
+    '26':  { 'name': "Risk", 'maxplayers': 5,'minplayers': 4, 'time': "long", 'kid':false, 'competitive':true},    
+    '27':  { 'name': "Saboteur", 'maxplayers': 10,'minplayers': 5, 'time': "medium", 'kid':true, 'competitive':true},    
+    '28':  { 'name': "Scrabble", 'maxplayers': 4,'minplayers': 2, 'time': "medium", 'kid':false, 'competitive':true},    
+    '29':  { 'name': "Sequence", 'maxplayers': 12,'minplayers': 3, 'time': "short", 'kid':true, 'competitive':true},    
+    '30':  { 'name': "Settlers of Catan", 'maxplayers': 4,'minplayers': 3, 'time': "medium", 'kid':true, 'competitive':true},    
+    '31':  { 'name': "Spot it!", 'maxplayers': 6,'minplayers': 4, 'time': "short", 'kid':true, 'competitive':true},    
+    '32':  { 'name': "Terraforming Mars", 'maxplayers': 5,'minplayers': 3, 'time': "long", 'kid':false, 'competitive':true},    
+    '33':  { 'name': "The Digital Game", 'maxplayers': 4,'minplayers': 2, 'time': "short", 'kid':true, 'competitive':true},    
+    '34':  { 'name': "The Grizzled", 'maxplayers': 5,'minplayers': 3, 'time': "short", 'kid':false, 'competitive':false},    
+    '35':  { 'name': "Ticket to Ride", 'maxplayers': 5,'minplayers': 4, 'time': "medium", 'kid':true, 'competitive':true},    
+    '36':  { 'name': "Twister", 'maxplayers': 4,'minplayers': 4, 'time': "short", 'kid':true, 'competitive':true},    
+    '37':  { 'name': "Uno Flip", 'maxplayers': 10,'minplayers': 3, 'time': "short", 'kid':true, 'competitive':true},    
+    '38':  { 'name': "Uno Stack", 'maxplayers': 10,'minplayers': 2, 'time': "short", 'kid':true, 'competitive':true},    
+    '39':  { 'name': "Unstable Unicorn", 'maxplayers': 8,'minplayers': 4, 'time': "medium", 'kid':false, 'competitive':true},    
+    '40':  { 'name': "Warewolf", 'maxplayers': 75,'minplayers': 7, 'time': "medium", 'kid':false, 'competitive':true},    
+    '41':  { 'name': "Duel", 'maxplayers': 2,'minplayers': 2, 'time': "medium", 'kid':false, 'competitive':true},    
 };
+
+'use strict';
+
+var inputField = document.querySelector('.chosen-value');
+var dropdown = document.querySelector('.value-list');
+var dropdownArray = [].concat(document.querySelectorAll('li'));
+var dropdownItems = dropdownArray[0];
+// dropdown.classList.add('open');
+inputField.focus(); // Demo purposes only
+
+var valueArray = [];
+dropdownItems.forEach(function (item) {
+  valueArray.push(item.textContent);
+});
+
+var closeDropdown = function closeDropdown() {
+  dropdown.classList.remove('open');
+};
+
+inputField.addEventListener('input', function () {
+  dropdown.classList.add('open');
+  var inputValue = inputField.value.toLowerCase();
+//   var valueSubstring = undefined;
+  if (inputValue.length > 0) {
+    for (var j = 0; j < valueArray.length; j++) {
+      if (!(inputValue.substring(0, inputValue.length) === valueArray[j].substring(0, inputValue.length).toLowerCase())) {
+        dropdownItems[j].classList.add('closed');
+      } else {
+        dropdownItems[j].classList.remove('closed');
+      }
+    }
+  } else {
+    for (var i = 0; i < dropdownItems.length; i++) {
+      dropdownItems[i].classList.remove('closed');
+    }
+  }
+});
+
+dropdownItems.forEach(function (item) {
+  item.addEventListener('click', function (evt) {
+    inputField.value = item.textContent;
+    dropdownItems.forEach(function (dropdown) {
+      dropdown.classList.add('closed');
+    });
+  });
+});
+
+inputField.addEventListener('focus', function () {
+  inputField.placeholder = 'Type a number';
+  dropdown.classList.add('open');
+  dropdownItems.forEach(function (dropdown) {
+    dropdown.classList.remove('closed');
+  });
+});
+
+inputField.addEventListener('blur', function () {
+  inputField.placeholder = 'How many players?';
+  dropdown.classList.remove('open');
+});
+
+document.addEventListener('click', function (evt) {
+  var isDropdown = dropdown.contains(evt.target);
+  var isInput = inputField.contains(evt.target);
+  if (!isDropdown && !isInput) {
+    dropdown.classList.remove('open');
+    var playerInputValue = Number(inputField.value[0]);
+    answers[1] = playerInputValue;
+    console.log(playerInputValue);
+  }
+});
 
 kidGames.addEventListener("click", function(){
     this.classList.add("buttonClick");
@@ -51,36 +155,6 @@ allPick.addEventListener("click", function(){
     console.log("showallGames");  
 });
 
-twoP.addEventListener("click", function(){
-    this.classList.add("buttonClick");
-    answers[1] = 2;
-    console.log(answers);
-    buttonOn3(fourP,sixP,this);
-    console.log("2 People");
-});
-
-fourP.addEventListener("click", function(){
-    this.classList.add("buttonClick");
-    answers[1] = 4;
-    console.log(answers);
-    buttonOn3(twoP,sixP,this);
-    console.log("4 or less People");
-});
-
-sixP.addEventListener("click", function(){
-    this.classList.add("buttonClick");
-    answers[1] = 6;
-    console.log(answers);
-    buttonOn3(fourP,twoP,this);
-    console.log("4-8 People");
-});
-
-// eightP.addEventListener("click", function(){
-//     this.classList.add("buttonClick");
-//     answers[1] = 8;
-//     console.log(answers);
-//     console.log("8 People");
-// });
 short.addEventListener("click", function(){
     this.classList.add("buttonClick");
     answers[0] = "short";
@@ -128,37 +202,47 @@ if (games.hasOwnProperty(item)) {
 }
 
 function searchKids(t,m,c,k) {
-    var price = arr.filter(function(item) { return item.time == t && item.maxplayers == m && item.kid == k && item.competitive == c; });
+    var price = arr.filter(function(item) { return item.time == t && item.maxplayers == m && item.minplayers <= m && item.kid == k && item.competitive == c; });
     chosen = Object.values(price);
     let result = chosen.map(a => a.name);
     spinTheWheel(result);
-    pickedCuisine.textContent = winner;
+    if (result.length >0) {
+        pickedCuisine.textContent = winner;
+        console.log("there are results")
+    }
+    else {
+        pickedCuisine.textContent = "No suitable game, please pick other options";
+        console.log("there are no results")
+    };
     console.log(result);
     console.log(price);
+    result = [];
 }
+
 function searchAll(t,m,c) {
-    var price = arr.filter(function(item) { return item.time == t && item.maxplayers == m && item.competitive == c; });
+    var price = arr.filter(function(item) { return item.time == t && item.maxplayers >= m && item.minplayers <= m && item.competitive == c; });
     chosen = Object.values(price);
     let result = chosen.map(a => a.name);
     spinTheWheel(result);
-    pickedCuisine.textContent = winner;
+    if (result.length >0) {
+        pickedCuisine.textContent = winner;
+        console.log("there are results")
+    }
+    else {
+        pickedCuisine.textContent = "No suitable game, please pick other options";
+        console.log("there are no results")
+    };
     console.log(result);
     console.log(price);
+    result = [];
 }
 
 function spinTheWheel(x){
     var result = Math.floor(Math.random() * x.length);
-    winner = x[result];
-    console.log(result);
-    return winner;
+        winner = x[result];
+        console.log(result);
+        return winner;
 }
-// function selectionDisplayToggle(x){
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-// }
 
 function buttonOn2(y,z){
     z.classList.add("buttonClick");
@@ -175,19 +259,15 @@ function buttonOn3(x,y,z){
 }
 
 function search() {
+    t= answers[0];
+    m= answers[1];
+    c= answers[2];
+    k= answers[3];
     if (answers.length==4) {
-    t=answers[0];
-    m=answers[1];
-    c=answers[2];
-    k=answers[3];
-    searchKids(t,m,c,k);
+        searchKids(t,m,c,k);
 }
-else{
-    t=answers[0];
-    m=answers[1];
-    c=answers[2];
-    k=answers[3];
-    searchAll(answers[0,1,2]);
+    else{
+        searchAll(t,m,c);
 }
 }
 
@@ -197,3 +277,4 @@ reset.addEventListener("click", function(){
         b.classList.remove("buttonClick");
     });
 });
+
---
+++ css-styled-and-filterable-select-dropdown/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/marijoha/pen/zKjvEw.
+
+ Select dropdown - styled and filterable using HTML, CSS and vanilla JS. Works using a input field to grab the value from the dropdown list. 
